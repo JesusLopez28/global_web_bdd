4196a138bfc8eca1ce8d0aee6bfceb8424af8b8d
JesusLopez28 <jesus.lopez280402@gmail.com>
2023-08-12 08:43:14 -0600

    Diseño

--- index.php
+++ index.php
@@ -5,11 +5,17 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Arsus</title>
+    <link rel="stylesheet" type="text/css" href="assets/css/styles.css">
+    <script src="https://kit.fontawesome.com/43cf0727bb.js" crossorigin="anonymous"></script>
 </head>
 
 <body>
-
+    <header>
+        <div class="headerBox">
+            <h1 class="headerTittle">ARSUS</h1>
+            
+        </div>
+    </header>
 </body>
-
 
 </html>
---
+++ views/login.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ARSUS - LOGIN</title>
+    <link rel="stylesheet" type="text/css" href="../assets/css/styles.css">
+    <script src="https://kit.fontawesome.com/43cf0727bb.js" crossorigin="anonymous"></script>
+</head>
+
+<body>
+    <div class="login-container">
+        <h1>Iniciar Sesión</h1>
+        <form action="#" method="post">
+            <input type="text" class="login-input" name="username" placeholder="Usuario" required>
+            <input type="password" class="login-input" name="password" placeholder="Contraseña" required>
+            <button type="submit" class="login-btn">Iniciar Sesión</button>
+        </form>
+    </div>
+</body>
+
+</html>
