c475551e12558c9f603a3f67294500abbc6c9da6
JesusLopez28 <jesus.lopez280402@gmail.com>
2023-08-12 19:51:28 -0600

    Trabajo con mi amore

--- index.php
+++ index.php
@@ -4,19 +4,14 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Arsus</title>
+    <title>ANSUS</title>
     <link rel="stylesheet" type="text/css" href="assets/css/styles.css">
 </head>
 
 <body>
-    <header>
-        <div class="headerBox">
-            <h1 class="headerTittle">Te amo mi amor</h1>
 
-        </div>
-    </header>
 
-    <script src="/assets/js/crud.js?x=1"></script>
+    <script src="assets/js/crud.js?x=1"></script>
 </body>
 
 </html>
--- views/login.php
+++ views/login.php
@@ -4,17 +4,22 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>ARSUS - LOGIN</title>
-    <link rel="stylesheet" type="text/css" href="../assets/css/styles.css">
+    <title>ANSUS - LOGIN</title>
+    <link rel="stylesheet" type="text/css" href="../assets/css/fonts.css">
+    <link rel="stylesheet" type="text/css" href="../assets/css/login.css">
 </head>
 
 <body>
     <div class="login-container">
+        <img src="../assets/images/x.png" class="avatar" alt="">
         <h1>Iniciar Sesión</h1>
         <form action="#" method="post">
             <input type="text" class="login-input" name="username" placeholder="Usuario" required>
             <input type="password" class="login-input" name="password" placeholder="Contraseña" required>
             <button type="submit" class="login-btn">Iniciar Sesión</button>
+            <div class="login-a">
+                <a href="#">Registrate</a>
+            </div>
         </form>
     </div>
 </body>
