b32165c6787d545fd8405fa3cee1fbaef05280af
JesusLopez28 <jesus.lopez280402@gmail.com>
2023-08-14 23:23:39 -0600

    Inicios del diseño

--- index.php
+++ index.php
@@ -5,13 +5,39 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>ANSUS</title>
+    <link rel="stylesheet" type="text/css" href="assets/css/fonts.css">
     <link rel="stylesheet" type="text/css" href="assets/css/styles.css">
+    <link rel="stylesheet" type="text/css" href="assets/css/footer.css">
+    <link rel="stylesheet" type="text/css" href="assets/css/alert.css">
+    <link rel="icon" href="assets/images/ansusIcon.png" type="image/x-icon">
 </head>
 
-<body>
+<body id="body">
 
-    <a href="views/login.php">Login</a>
+    <header>
+        <div class="icon__menu">
+            <img src="assets/icons/menu.png" id="btn_open" alt="Menú">
+        </div>
+    </header>
+
+    <div class="menu__side" id="menu_side">
+        <div class="name__page">
+            <img src="assets/icons/ansus.png" alt="Ansus">
+            <h4>ANSUS</h4>
+        </div>
+
+        <div class="options__menu">
+            <a href="#" class="selected">
+                <div class="option">
+                    <img src="assets/icons/carrito.png" alt="carrito">
+                    <h4>Carrito</h4>
+                </div>
+            </a>
+        </div>
+    </div>
+
     <script src="assets/js/crud.js?x=1"></script>
+    <script src="assets/js/lateral.js"></script>
 </body>
 
 </html>
