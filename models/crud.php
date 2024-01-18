c75d1e724a348d5be3146dd7b7c9c41e706191fd
root <jesus.lopez280402@gmail.com>
2024-01-18 17:13:02 -0600

    ya

--- models/crud.php
+++ models/crud.php
@@ -386,12 +386,17 @@ class Crud
         $pdfFilePath = 'pdf/order_' . $orderId . '.pdf';
         if (DEBUG) echo "\n$pdfFilePath\n";
 
-        if ($pdf->Output($pdfFilePath, 'F')) {
-            if (DEBUG) echo "SI";
-        } else {
-            if (DEBUG) echo "NO";
+        try {
+            if ($pdf->Output($pdfFilePath, 'F')) {
+                if (DEBUG) echo "SI";
+            } else {
+                if (DEBUG) echo "NO";
+            }
+            if (DEBUG) echo "4";
+        } catch (Exception $e) {
+            if (DEBUG) echo 'Excepción capturada: ',  $e->getMessage(), "\n";
         }
-        if (DEBUG) echo "4";
+
         return $pdfFilePath;
     }
 
